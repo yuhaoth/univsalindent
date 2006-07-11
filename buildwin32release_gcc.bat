ccab292805a8cd6bd1898d92900af22dea7ea7ef
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2006-07-11 20:26:44 +0000

    Added build files for win32 and source

---
+++ buildwin32release_gcc.bat
@@ -0,0 +1,30 @@
+md UniversalIndentGUI_win32
+cd UniversalIndentGUI_win32
+md data
+cd ..
+copy .\data\.astylerc .\UniversalIndentGUI_win32\data
+copy .\data\AStyle.exe .\UniversalIndentGUI_win32\data
+copy .\data\astyle.html .\UniversalIndentGUI_win32\data
+copy .\data\indent.exe .\UniversalIndentGUI_win32\data
+copy .\data\indent.txt .\UniversalIndentGUI_win32\data
+copy .\data\.indent.pro .\UniversalIndentGUI_win32\data
+copy .\data\libiconv-2.dll .\UniversalIndentGUI_win32\data
+copy .\data\libintl-2.dll .\UniversalIndentGUI_win32\data
+copy .\data\gc.cfg .\UniversalIndentGUI_win32\data
+copy .\data\gc.exe .\UniversalIndentGUI_win32\data
+copy .\data\gc.txt .\UniversalIndentGUI_win32\data
+copy .\data\uigui_astyle.ini .\UniversalIndentGUI_win32\data
+copy .\data\uigui_gnuindent.ini .\UniversalIndentGUI_win32\data
+copy .\data\uigui_greatcode.ini .\UniversalIndentGUI_win32\data
+copy .\data\example.cpp .\UniversalIndentGUI_win32\data
+copy .\changelog.txt .\UniversalIndentGUI_win32\
+copy .\LICENSE.GPL .\UniversalIndentGUI_win32\
+copy .\QtCore4.dll .\UniversalIndentGUI_win32\
+copy .\QtGui4.dll .\UniversalIndentGUI_win32\
+copy .\mingwm10.dll .\UniversalIndentGUI_win32\
+copy .\readme.txt .\UniversalIndentGUI_win32\
+copy .\release\UniversalIndentGUI.exe .\UniversalIndentGUI_win32\
+cd UniversalIndentGUI_win32
+7z.exe a -tzip UniversalIndentGUI_0.2.1.1_Alpha_win32.zip
+cd ..
+pause
