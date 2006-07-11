d51508a930572ce3f54a7c85ea63a0045ce888b6
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2006-07-11 20:25:48 +0000

    Added build file for linux

---
+++ buildLinuxRelease.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo calling rm -r release/debug
+echo ---------------------------
+rm -r release
+rm -r debug
+echo calling qmake
+echo -------------
+qmake
+echo calling make release
+echo --------------------
+make release
+echo calling rm -r UniversalIndentGUI_linux32
+echo ----------------------------------------
+rm -r UniversalIndentGUI_linux32
+echo calling mkdir UniversalIndentGUI_linux32
+echo ----------------------------------------
+mkdir UniversalIndentGUI_linux32
+mkdir UniversalIndentGUI_linux32/data
+cp ./changelog.txt ./UniversalIndentGUI_linux32/
+cp ./LICENSE.GPL ./UniversalIndentGUI_linux32/
+cp ./readme.txt ./UniversalIndentGUI_linux32/
+cp ./debug/UniversalIndentGUI ./UniversalIndentGUI_linux32/
+cp ./data/.astylerc ./UniversalIndentGUI_linux32/data/
+cp ./data/astyle ./UniversalIndentGUI_linux32/data/
+cp ./data/astyle.html ./UniversalIndentGUI_linux32/data/
+cp ./data/gc.cfg ./UniversalIndentGUI_linux32/data/
+cp ./data/gc.exe ./UniversalIndentGUI_linux32/data/
+cp ./data/gc.txt ./UniversalIndentGUI_linux32/data/
+cp ./data/uigui_astyle.ini ./UniversalIndentGUI_linux32/data/
+cp ./data/uigui_greatcode.ini ./UniversalIndentGUI_linux32/data/
+cp ./data/uigui_gnuindent.ini ./UniversalIndentGUI_linux32/data/
+cp ./data/example.cpp ./UniversalIndentGUI_linux32/data/
+cd UniversalIndentGUI_linux32
+tar czf UniversalIndentGUI_0.2.1_Alpha_linux32.tgz *
+cd ..
+read -p "press any key to continue"
