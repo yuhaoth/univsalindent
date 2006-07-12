af2f1c8d6298664e1ae85a862cb31182feeded2b
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2006-07-12 08:51:10 +0000

    Updated some directories and added gnu indent.

--- buildLinuxRelease.sh
+++ buildLinuxRelease.sh
@@ -16,21 +16,28 @@ echo calling mkdir UniversalIndentGUI_linux32
 echo ----------------------------------------
 mkdir UniversalIndentGUI_linux32
 mkdir UniversalIndentGUI_linux32/data
+echo copying files to UniversalIndentGUI_linux32
+echo ----------------------------------------
 cp ./changelog.txt ./UniversalIndentGUI_linux32/
 cp ./LICENSE.GPL ./UniversalIndentGUI_linux32/
 cp ./readme.txt ./UniversalIndentGUI_linux32/
-cp ./debug/UniversalIndentGUI ./UniversalIndentGUI_linux32/
+cp ./release/UniversalIndentGUI ./UniversalIndentGUI_linux32/
 cp ./data/.astylerc ./UniversalIndentGUI_linux32/data/
 cp ./data/astyle ./UniversalIndentGUI_linux32/data/
 cp ./data/astyle.html ./UniversalIndentGUI_linux32/data/
 cp ./data/gc.cfg ./UniversalIndentGUI_linux32/data/
 cp ./data/gc.exe ./UniversalIndentGUI_linux32/data/
 cp ./data/gc.txt ./UniversalIndentGUI_linux32/data/
+cp ./data/.indent.pro ./UniversalIndentGUI_linux32/data/
+cp ./data/indent ./UniversalIndentGUI_linux32/data/
+cp ./data/indent.html ./UniversalIndentGUI_linux32/data/
 cp ./data/uigui_astyle.ini ./UniversalIndentGUI_linux32/data/
 cp ./data/uigui_greatcode.ini ./UniversalIndentGUI_linux32/data/
 cp ./data/uigui_gnuindent.ini ./UniversalIndentGUI_linux32/data/
 cp ./data/example.cpp ./UniversalIndentGUI_linux32/data/
 cd UniversalIndentGUI_linux32
-tar czf UniversalIndentGUI_0.2.1_Alpha_linux32.tgz *
+echo compressing UniversalIndentGUI_linux32
+echo ----------------------------------------
+tar czf UniversalIndentGUI_0.2.2_Alpha_linux32.tgz *
 cd ..
 read -p "press any key to continue"
