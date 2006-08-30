020637d3d2781793c0c43da1a9e74da563758338
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2006-08-30 18:26:27 +0000

    minor changes. If ever used again...

--- makeall.bat
+++ makeall.bat
@@ -1,4 +1,5 @@
 qmake
+nmake clean
 nmake all
-mt -manifest .\release\UniversalIndentGUI.exe.manifest -outputresource:".\release\UniversalIndentGUI.exe;#1"
-mt -manifest .\debug\UniversalIndentGUI.exe.manifest -outputresource:".\debug\UniversalIndentGUI.exe;#1"
+rem mt -manifest .\release\UniversalIndentGUI.exe.manifest -outputresource:".\release\UniversalIndentGUI.exe;#1"
+rem mt -manifest .\debug\UniversalIndentGUI.exe.manifest -outputresource:".\debug\UniversalIndentGUI.exe;#1"
