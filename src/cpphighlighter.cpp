27e1dcfaf461153bab92619980b4f0488690e46a
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2007-02-17 08:29:31 +0000

    Moved all syntax highlight code to the cpphighlighter class, which will be renamed to highlighter. Cleaned up the code a bit.

--- src/cpphighlighter.cpp
+++ src/cpphighlighter.cpp
@@ -29,14 +29,12 @@
 /*!
     The constructor initializes some regular expressions and keywords to identify cpp tokens
  */
-CppHighlighter::CppHighlighter(QTextEdit *parent)
-: QSyntaxHighlighter(parent)
+CppHighlighter::CppHighlighter(QsciScintilla *parent)
+: QObject(parent)
 {
     this->parent = parent;
-    HighlightingRule rule;
-
     highlightningIsOn = true;
-
+/*
     keywordFormat.setForeground(Qt::darkBlue);
     keywordFormat.setFontWeight(QFont::Bold);
     QStringList keywordPatterns;
@@ -80,49 +78,17 @@ CppHighlighter::CppHighlighter(QTextEdit *parent)
     rule.pattern = QRegExp("\\b[A-Za-z0-9_]+(?=\\()");
     rule.format = functionFormat;
     highlightingRules.append(rule);
-
-    commentStartExpression = QRegExp("/\\*");
-    commentEndExpression = QRegExp("\\*/");
-}
-
-/*!
-    This function is automatically called by the connected QTextEdit object if needed. \a text
-    is a pointer to the QString which is parsed for keywords and other syntax elements to highlight.
-    This function needs to be optimized because it scans the whole text for each keyword/RegExp
 */
-void CppHighlighter::highlightBlock(const QString &text)
-{
-    if ( highlightningIsOn ) {
-        foreach (HighlightingRule rule, highlightingRules) {
-            QRegExp expression(rule.pattern);
-            int index = text.indexOf(expression);
-            while (index >= 0) {
-                int length = expression.matchedLength();
-                setFormat(index, length, rule.format);
-                index = text.indexOf(expression, index + length);
-            }
-        }
-        setCurrentBlockState(0);
+    //commentStartExpression = QRegExp("/\\*");
+    //commentEndExpression = QRegExp("\\*/");
 
-        int startIndex = 0;
-        if (previousBlockState() != 1)
-            startIndex = text.indexOf(commentStartExpression);
+	lexer = new QsciLexerCPP(parent);
+	parent->setLexer(lexer);
 
-        while (startIndex >= 0) {
-            int endIndex = text.indexOf(commentEndExpression, startIndex);
-            int commentLength;
-            if (endIndex == -1) {
-                setCurrentBlockState(1);
-                commentLength = text.length() - startIndex;
-            } else {
-                commentLength = endIndex - startIndex
-                    + commentEndExpression.matchedLength();
-            }
-            setFormat(startIndex, commentLength, multiLineCommentFormat);
-            startIndex = text.indexOf(commentStartExpression,
-                startIndex + commentLength);
-        }
-    }
+	lexer->setDefaultFont( QFont("Courier", 10) );
+	lexer->setFont( QFont("Courier", 10), QsciLexerCPP::CommentLine );
+	lexer->setFont( QFont("Courier", 10), QsciLexerCPP::Comment );
+	lexer->refreshProperties();
 }
 
 /*!
@@ -130,6 +96,7 @@ void CppHighlighter::highlightBlock(const QString &text)
 */
 void CppHighlighter::turnHighlightOn() {
     highlightningIsOn = true;
+	parent->setLexer(lexer);
 }
 
 /*!
@@ -137,32 +104,5 @@ void CppHighlighter::turnHighlightOn() {
 */
 void CppHighlighter::turnHighlightOff() {
     highlightningIsOn = false;
-/*  // Some tests on changing the style of the textedit for export with syntax highlight.
-    this->parent->setHtml("<html>Thomas ist public toll!<br> Und ich auch.</html>");
-    QString text = this->parent->toPlainText();
-    QTextCursor cursor = this->parent->textCursor();
-    QTextCharFormat fmt;
-    fmt.setFontWeight(QFont::Light);
-    fmt.setForeground(Qt::red);
-    cursor.setCharFormat(fmt);
-    this->parent->setCurrentCharFormat(fmt);
-
-    foreach (HighlightingRule rule, highlightingRules) {
-        QRegExp expression(rule.pattern);
-        int index = text.indexOf(expression);
-        while (index >= 0) {
-            int length = expression.matchedLength();
-            bool b = cursor.hasSelection();
-            cursor.setPosition(index);
-            cursor.setPosition(index+length, QTextCursor::KeepAnchor);
-            b = cursor.hasSelection();
-            int s = cursor.selectionStart();
-            s = cursor.selectionEnd();
-            cursor.mergeCharFormat(fmt);
-            this->parent->setTextCursor(cursor);
-            this->parent->mergeCurrentCharFormat(rule.format);
-            index = text.indexOf(expression, index + length);
-        }
-    }
-*/
+	parent->setLexer(0);
 }
