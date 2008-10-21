b4b7ea1785b592bef1c5de913a492d012c334272
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2008-10-21 15:41:51 +0000

    Updated the web page. Updated a linux screen shot. Added lightbox for showing the screenshots (does not work properly with opera yet). Moved the original css file into a subdir.

--- website/screenshots.php
+++ website/screenshots.php
@@ -23,31 +23,31 @@ include("htmlhead.html");
 			<div id="content">
 				<h2>Screenshots</h2>
 				<br />
-				<a href="images/screenshot1.png">
+				<a href="images/screenshot1.png" rel="lightbox[uiguiscreenshots]" title="UniversalIndentGUI under XP.">
                     <img src="images/screenshot1.jpg" alt="WinXPScreenshot1" width="500"/>
                 </a>
 				<p>UniversalIndentGUI under XP.</p>
 
 				<br />
-				<a href="images/screenshot2.png">
+				<a href="images/screenshot2.png" rel="lightbox[uiguiscreenshots]" title="UniversalIndentGUI under XP showing up a tooltip.">
                     <img src="images/screenshot2.jpg" alt="WinXPScreenshot2" width="500"/>
                 </a>
 				<p>UniversalIndentGUI under XP showing up a tooltip.</p>
 
 				<br />
-				<a href="images/screenshot3.png">
-                    <img src="images/screenshot3.jpg" alt="LinuxScreenshot3" width="500"/>
+				<a href="images/screenshot3.png" rel="lightbox[uiguiscreenshots]" title="UniversalIndentGUI under Linux showing the indenter select menu.">
+                    <img src="images/screenshot3.jpg" alt="LinuScreenshot3" width="500"/>
                 </a>
 				<p>UniversalIndentGUI under Linux showing the indenter select menu.</p>
 				
 				<br />
-				<a href="images/screenshot4.png">
+				<a href="images/screenshot4.png" rel="lightbox[uiguiscreenshots]" title="UniversalIndentGUI under Linux with outdocked indenter parameter menu.">
                     <img src="images/screenshot4.jpg" alt="LinuxScreenshot4" width="500"/>
                 </a>
 				<p>UniversalIndentGUI under Linux with outdocked indenter parameter menu.</p>
 				
 				<br />
-				<a href="images/screenshot5.png">
+				<a href="images/screenshot5.png" rel="lightbox[uiguiscreenshots]" title="The about dialog of UniversalIndentGUI.">
                     <img src="images/screenshot5.jpg" alt="AboutDialogScreenshot5" width="500"/>
                 </a>
 				<p>The about dialog of UniversalIndentGUI.</p>
